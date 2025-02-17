a2dfc92a51f1505d034f632c1ac60fbe225fee46
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-02-17 16:19:43 +0300

    task 17/2/2025 (^-^)

---
+++ task_17_2_2018/task_17_2_2018/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace task_17_2_2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
