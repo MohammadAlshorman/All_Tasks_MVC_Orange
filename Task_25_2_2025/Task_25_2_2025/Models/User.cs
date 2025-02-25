caa3b5c1942084ebdee058d894fd29468e4acb5b
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-02-25 10:49:52 +0300

    ADD Task 25_2_2025

---
+++ Task_25_2_2025/Task_25_2_2025/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_25_2_2025.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+}
